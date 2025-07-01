97247c6ebb0889e3bd7ff0cf602bd0f415185555
Raashi25 <raashihl25@gmail.com>
2025-07-02 00:50:51 +0530

    commiting externalref

---
+++ Tourisms.java
@@ -0,0 +1,13 @@
+class Tourisms {
+	static String tajmahal="Taj Mahal";
+	static String minar="Qutub Minar";
+	static String jaipur="Jaipur";
+	
+
+     static String[] touristPlaces={tajmahal, minar, jaipur," Varanasi"," Goa"," Manali"," Shimla"," Leh-Ladakh"," Darjeeling"," Rameswaram"};
+	 public static void main(String[] places){
+       System.out.println(tajmahal);
+	   System.out.println(minar);
+       System.out.println(jaipur);
+    }
+}   
