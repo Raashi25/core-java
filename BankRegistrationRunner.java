ff804639b65840d3386544df9520e5007c083b48
Raashi25 <raashihl25@gmail.com>
2025-07-28 10:55:05 +0530

    constructors

--- BankRegistrationRunner.java
+++
@@ -1,7 +0,0 @@
-class BankRegistrationRunner{
-	public static void main(String []br){
-		boolean isregistrationDone = BankRegistration.registration("Raashi" ,false,"25-10-2004","Shivamogga",483073567313l);
-		System.out.println(" the  BankRegistration is "+isregistrationDone);
-		if(isregistrationDone==true)BankRegistration.getDetails();
-		
-}}
